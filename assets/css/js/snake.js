ff42b12e3e0a1828e174820269c07fb69ff6ad66
Andre <sarreqiandrea@gmail.com>
2022-12-21 02:05:43 +0000

    Add Wall borders so the snake can't loop through them and add Game Over rules

--- assets/css/js/snake.js
+++ assets/css/js/snake.js
@@ -10,7 +10,7 @@ class snakePart {
     }
 }
 
-let tileCount = 20;
+let tileCount = 25;
 let tileSize = canvas.width / tileCount - 2;
 
 let headX = 10;
@@ -24,11 +24,21 @@ let appleY = 5;
 let xVelocity = 0;
 let yVelocity = 0;
 
-let score = 0;
+var width = canvas.width;
+var height = canvas.height;
+var score = 0;
+
+var blockSize = 10;
+var widthInBlocks = width / blockSize;
+var heightInBlocks = height / blockSize;
 
 //Game LOOP
 function drawGame() {
-    changeSnakePosition();
+    changeSnakePosition(); 
+    let result = isGameOver();
+    if (result){
+        return;
+    }
 
     checkAppleCollision();
     drawScore()
@@ -36,13 +46,40 @@ function drawGame() {
     drawApple();
 
     drawSnake();
-   gameOver();
+ 
     drawScore();
+    if (score > 5) {
+        speed = 9;
+      }
+      if (score > 10) {
+        speed = 15;
+      }
+      if (score > 15) {
+        speed = 20;
+      }
+      if (score > 20) {
+        speed = 25;
+      }
+      if (score > 30) {
+        speed = 30;
+      }
 setTimeout (drawGame, 1000 / speed);
 }
-
-function gameOver () {
-
+function isGameOver(){
+    let gameOver=false;
+    
+    if(headX < 0) {//if snake hits left wall
+        alert("Game Over");
+    }
+    else if(headX===tileCount) {//if snake hits right wall
+        alert("Game Over");
+    }
+    else if(headY < 0) {//if snake hits wall at the top
+        alert("Game Over");
+    }
+    else if(headY === tileCount) {//if snake hits wall at the bottom
+        alert("Game Over"); 
+    }
 }
 
 //score 
@@ -58,12 +95,27 @@ ctx.fillRect(0, 0, canvas.height, canvas.width);
 }
 
 function drawSnake() {
+    ctx.fillStyle = "green";
+    for (let i = 0; i < snakeParts.length; i++) {
+      let part = snakeParts[i];
+      ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
+    }
+    if (headY < 0) {
+        headY = canvas.height - grid;
+      }
+      else if (headY >= canvas.height) {
+        headY = 0;
+      }
+      if (headX < 0) {
+        headX = canvas.width - grid;
+      }
+      else if (headX >= canvas.width) {
+        headX = 0;
+      }
+    
     ctx.fillStyle = "orange";
     ctx.fillRect(headX * tileCount, headY * tileCount, tileSize, tileSize);
-
-    ctx.fillStyle = "green";
-    
-}
+  }
 
 function changeSnakePosition() {
     headX = headX + xVelocity; 
@@ -85,6 +137,7 @@ function checkAppleCollision() {
 }
 
 document.addEventListener('keydown', keyDown)
+
 //up key
 function keyDown (event) {
     //up key
